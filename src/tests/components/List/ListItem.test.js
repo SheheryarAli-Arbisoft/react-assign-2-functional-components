4976dcaa6008d335680c40d01d9fab4e03adc7a4
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-14 18:25:47 +0500

    Added tests for the list components and modified some test files

--- src/tests/components/List/ListItem.test.js
+++ src/tests/components/List/ListItem.test.js
@@ -4,28 +4,12 @@ import { connectProviders } from '../../utils';
 
 describe('testing the ListItem component', () => {
   test('should render the component without errors', () => {
-    const video = {
-      thumbnails: {
-        high: {
-          url: 'some url',
-        },
-      },
-      title: 'Title',
-      channelTitle: 'Channel Title',
-      publishedAt: 'Monday',
-      description: 'Description',
+    const weather = {
+      name: 'Rawalpindi',
+      forecast: [],
     };
-    const wrapper = mount(connectProviders(ListItem, { video }));
-    const thumbnail = wrapper.find('div img');
+    const wrapper = mount(connectProviders(ListItem, { weather }));
     const container = wrapper.find('div div');
-    expect(thumbnail.at(0).props().src).toBe(video.thumbnails.high.url);
-    expect(container.at(0).children().at(0).text()).toBe(video.title);
-    expect(container.at(0).children().at(1).children().at(0).text()).toBe(
-      video.channelTitle
-    );
-    expect(container.at(0).children().at(1).children().at(1).text()).not.toBe(
-      ''
-    );
-    expect(container.at(0).children().at(2).text()).toBe(video.description);
+    expect(container.length).toBe(11);
   });
 });
